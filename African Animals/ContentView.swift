c68686bb75244cb6805866826db6f5ab139dbd0e
princebottomancodes <103578263+princebottomancodes@users.noreply.github.com>
2022-06-19 19:04:47 +0200

    Initial Commit

---
+++ African Animals/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  African Animals
+//
+//  Created by Prince Bottoman on 2022/06/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
